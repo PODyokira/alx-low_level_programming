57538e07dbc8f3796a2600183950b19924275e0f
PODyokira <yassineel387@gmail.com>
2023-04-24 17:51:18 -0700

    Commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,13 @@
+/**
+ * _strcpy - copies a string from one pointer to another
+ *	including the terminating null byte (\0)
+ *
+ * @src: source of string parameter input
+ * @dest: destination of string
+ *
+ * Return: pointer to dest input parameter
+*/
+
 char *_strcpy(char *dest, char *src)
 {
 	int a, b = 0;
